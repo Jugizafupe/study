5a546b743f3c7880a9dfac4df95dc5e836a0f0ff
Jugizafupe <72652049+Jugizafupe@users.noreply.github.com>
2022-03-03 09:03:03 +0300

    Add files via upload

---
+++ 8solve.py
@@ -0,0 +1,8 @@
+from itertools import product
+p=product('ИГОРЬ', repeat=8)
+k=0
+for x in p:
+    s=''.join(x)
+    if (s[0]!='Ь') and (s.count('О')==1) and (s.count('Ь')==1):
+        k+=1
+print(k)
